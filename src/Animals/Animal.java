2657161d11678b608202f741bb281ba7c82c82a2
HaimA16 <haimarmiass@gmail.com>
2024-06-27 15:54:32 +0300

    adding

--- src/Animals/Animal.java
+++ src/Animals/Animal.java
@@ -23,10 +23,29 @@ public abstract class Animal extends Mobile {
         this.weight = weight;
         this.speed = speed;
         this.medals = new Medal[medals.length];
+        for (int i = 0; i < medals.length; i++) {
+            if (medals[i] == null) {
+                throw new IllegalArgumentException("Medal cannot be null!");
+            }
+            this.medals[i] = medals[i];
+        }
 
     }
-    public void makeSound(){
+    /*public void makeSound(){
         System.out.println("Animal " + name + " said ");
+    }*/
+
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof Animal))
+            return false;
+        Animal other = (Animal) obj;
+        return this.name.equals(other.name) &&
+                this.gender == other.gender &&
+                this.weight == other.weight &&
+                this.speed == other.speed &&
+                Arrays.equals(this.medals, other.medals);
     }
 
     @Override
